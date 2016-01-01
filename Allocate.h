84ce6b4ba4541c89ea99c039754abb5dce05d648
Devin Waas <dsc.waas@gmail.com>
2016-01-01 22:33:08 +0100

    caused segfault with wrong usage message

--- Allocate.h
+++ Allocate.h
@@ -6,40 +6,46 @@
 #include "debug.h"
 
 #define FREE(p)										\
-			free (p);								\
-			p = NULL; 
+                free (p);						                \
+                p = NULL;                                                               \
 
-#define FREE2D(p,size_x)							\
-			for (unsigned i = 0; i < (size_x); ++i)	\
-			{ 										\
-				FREE (p[i]);						\
-			}										\
-			FREE (p);
+#define FREE2D(p,size_x)							        \
+                        if (p)                                                          \
+                        {                                                               \
+                                for (unsigned i = 0; i < (size_x); ++i)	                \
+                                { 							\
+                                        FREE (p[i]);					\
+                                }							\
+                                FREE (p);                                               \
+                        }
 
-#define FREE3D(p,size_y,size_x)						\
-			for (unsigned j = 0; j < (size_x); ++j)	\
-			{ 										\
-				FREE2D (p[j], size_y);				\
-			}										\
-			FREE(p);								
+#define FREE3D(p,size_y,size_x)						                \
+                        if (p)                                                          \
+                        {                                                               \
+                                for (unsigned j = 0; j < (size_x); ++j)	                \
+                                { 							\
+                                        FREE2D (p[j], size_y);			        \
+                                }							\
+                                FREE(p);						\
+                        }
 
-#define ALLOC(p,size_x)								\
+#define ALLOC(p,size_x)							        	\
 			if (p) free (p);						\
-			p = calloc ((size_x), sizeof(*p));		\
+			p = calloc ((size_x), sizeof(*p));		                \
 			check_mem (p);
 
-#define ALLOC2D(p,size_x,size_y)					\
+#define ALLOC2D(p,size_x,size_y)					                \
 			ALLOC(p,(size_x));						\
-			for (unsigned i = 0; i < (size_x); ++i)	\
-			{										\
-				ALLOC (p[i], (size_y) );			\
+			for (unsigned i = 0; i < (size_x); ++i)	                        \
+			{								\
+				ALLOC (p[i], (size_y) );			        \
 			}																		
 
-#define ALLOC3D(p,size_x,size_y,size_z)				\
-			ALLOC (p, (size_x) );					\
-			for (unsigned j = 0; j < (size_x); ++j)	\
-			{										\
-				ALLOC2D (p[j], (size_y), (size_z) );\
+#define ALLOC3D(p,size_x,size_y,size_z)				                        \
+			ALLOC (p, (size_x) );					        \
+			for (unsigned j = 0; j < (size_x); ++j)	                        \
+			{								\
+				ALLOC2D (p[j], (size_y), (size_z) );                    \
 			}													
 
 #endif /* ALLOCATE_H */
--- main.c
+++ main.c
@@ -20,212 +20,213 @@ main
 	char *argv[]
 )
 {
-	check (
-			argc == 2,
-			"\nNumber of arguments is wrong! \
-			\nusage: ./post_proc working_directory_path!!!!\n\n"
-		  );
+        //declare
+        KraichnanMode*** kraich_modes = NULL;
 
-    char work_dir[40];
-	sprintf (work_dir, "%s", argv[1]);
+        Molecule** positions = NULL;
+        TurbField** turb_velocities = NULL;
+        Tensor2** strain_rate = NULL;    
 
-    //CONSTANTS
-    MDConstants K;
-    char input_dat[60];
-	sprintf (input_dat, "%s/input.dat", work_dir);
-	check 
-		(
-			!Initialize (&K, input_dat), 
-			"Constant init failed" 
-		);
+        TurbConsts* turb = NULL;
+        TurbConstVecs* turb_vecs = NULL;
 
-    //declare
-    KraichnanMode*** kraich_modes = NULL;
 
-    Molecule** positions = NULL;
-    TurbField** turb_velocities = NULL;
-    Tensor2** strain_rate = NULL;    
+        check (
+                        argc == 2,
+                        "\nNumber of arguments is wrong! \
+                        \nusage: ./post_proc working_directory_path!!!!\n\n"
+              );
 
-    TurbConsts* turb = NULL;
-    TurbConstVecs* turb_vecs = NULL;
+        char work_dir[40];
+        sprintf (work_dir, "%s", argv[1]);
 
-   //allocate 
-    ALLOC3D (kraich_modes, K.SnapshotNum, K.PartNum, K.NF);
+        //CONSTANTS
+        MDConstants K;
+        char input_dat[60];
+        sprintf (input_dat, "%s/input.dat", work_dir);
+        check 
+                (
+                 !Initialize (&K, input_dat), 
+                 "Constant init failed" 
+                );
 
-    ALLOC2D (positions, K.SnapshotNum, K.PartNum);
-    ALLOC2D (turb_velocities, K.SnapshotNum, K.PartNum);
-    ALLOC2D (strain_rate, K.SnapshotNum, K.PartNum);
+        //allocate 
+        ALLOC3D (kraich_modes, K.SnapshotNum, K.PartNum, K.NF);
 
-    ALLOC (turb, K.NF);
-    ALLOC (turb_vecs, K.NF);
+        ALLOC2D (positions, K.SnapshotNum, K.PartNum);
+        ALLOC2D (turb_velocities, K.SnapshotNum, K.PartNum);
+        ALLOC2D (strain_rate, K.SnapshotNum, K.PartNum);
 
-    //Initialise turb constants
-    char turb_pos[40];
-    sprintf(turb_pos, "%s/turbulence.pos", work_dir);
+        ALLOC (turb, K.NF);
+        ALLOC (turb_vecs, K.NF);
 
-    check
-		(
-			!TurbConstsLoad 
-			(   
-				K,
-				turb_pos,
-				turb,
-				turb_vecs
-			),
-		"TurbConstLoad failed"
-		);
-    //TODO check headers
+        //Initialise turb constants
+        char turb_pos[40];
+        sprintf(turb_pos, "%s/turbulence.pos", work_dir);
 
-    //initialise turb constants ends
+        check
+                (
+                 !TurbConstsLoad 
+                 (   
+                  K,
+                  turb_pos,
+                  turb,
+                  turb_vecs
+                 ),
+                 "TurbConstLoad failed"
+                );
+        //TODO check headers
 
-    //file reading starts
-    fprintf(stderr, "\nReading data files...     ");
+        //initialise turb constants ends
 
-    Tensor2 meanS = {{0}};
-    for (unsigned t = 0, n = 0;  t < K.iteration_num; t += K.t_gap) 
-    {
-        assert (n < K.SnapshotNum);
-        fprintf(stderr, "\n%lf%%", 100.0*(double)n/(double)K.SnapshotNum); 
-        fflush(NULL);
-        //TODO load turbulence.pos only once!!
+        //file reading starts
+        fprintf(stderr, "\nReading data files...     ");
 
-        char fname[60];
-        sprintf (fname, "%s/t-%d.pos", work_dir, t);
-		
-		check
-		(
-       		!MDLoadPos
-            (
-             K,
-             fname,
-             positions[n] 
-            ),
-			"MDLoadPos failed"
-		);
-      
-       sprintf (fname, "%s/t-%d.pos", work_dir, t);
-	   
-	   check
-	   (
-       		!MDLoadDir
-            (
-             K,
-             fname,
-             positions[n] 
-            ),
-			"MDLoadDir failed"
-       );
-       sprintf (fname, "%s/turbField-%d.pos", work_dir, t);
-
-	   check
-	   (
-       		!MDLoadTurb
-            (
-             K,
-             fname,
-             turb_velocities[n] 
-            ),
-			"MDLoadTurb failed"
-		);
-
-		check
-		(
-        	 !InitializeTurbModes
-             (
-                K,
-                positions[n],
-                turb_vecs,
-                turb,
-                kraich_modes[n],
-                t
-             ),
-			 "InitializeTurbModes failed"
-		);
-/*
-        if ( n > 0)
+        Tensor2 meanS = {{0}};
+        for (unsigned t = 0, n = 0;  t < K.iteration_num; t += K.t_gap) 
         {
-            check
-			( 
-				 !InitializeTurbVelocities
-                 (
+                assert (n < K.SnapshotNum);
+                fprintf(stderr, "\n%lf%%", 100.0*(double)n/(double)K.SnapshotNum); 
+                fflush(NULL);
+                //TODO load turbulence.pos only once!!
+
+                char fname[60];
+                sprintf (fname, "%s/t-%d.pos", work_dir, t);
+
+                check
+                        (
+                         !MDLoadPos
+                         (
+                          K,
+                          fname,
+                          positions[n] 
+                         ),
+                         "MDLoadPos failed"
+                        );
+
+                sprintf (fname, "%s/t-%d.pos", work_dir, t);
+
+                check
+                        (
+                         !MDLoadDir
+                         (
+                          K,
+                          fname,
+                          positions[n] 
+                         ),
+                         "MDLoadDir failed"
+                        );
+                sprintf (fname, "%s/turbField-%d.pos", work_dir, t);
+
+                check
+                        (
+                         !MDLoadTurb
+                         (
+                          K,
+                          fname,
+                          turb_velocities[n] 
+                         ),
+                         "MDLoadTurb failed"
+                        );
+
+                check
+                        (
+                         !InitializeTurbModes
+                         (
+                          K,
+                          positions[n],
+                          turb_vecs,
+                          turb,
+                          kraich_modes[n],
+                          t
+                         ),
+                         "InitializeTurbModes failed"
+                        );
+                /*
+                   if ( n > 0)
+                   {
+                   check
+                   ( 
+                   !InitializeTurbVelocities
+                   (
                    K,
                    turb_vecs,
-    			   (const KraichnanMode**) kraich_modes[n],
+                   (const KraichnanMode**) kraich_modes[n],
                    turb_velocities[n] 
-                 ),
-				 "InitializeTurbVelocities failed"
-			 );
+                   ),
+                   "InitializeTurbVelocities failed"
+                   );
+                   }
+                   */
+                //FIXME generated turb velocities are not the same as the ones contained in the .pos!
+
+                //init strain rate tensor
+                for (unsigned i = 0; i < K.PartNum; ++i)
+                {
+                        InitializeStrainRateTensor
+                                (
+                                 strain_rate[n][i],
+                                 K,
+                                 turb_vecs,
+                                 kraich_modes[n][i]
+                                );
+                } 
+                n++;
         }
-*/
-//FIXME generated turb velocities are not the same as the ones contained in the .pos!
-
-//init strain rate tensor
-        for (unsigned i = 0; i < K.PartNum; ++i)
-        {
-            InitializeStrainRateTensor
+        //file reading ends
+        //calc kinetic energy
+        double mean_kinetic_energy =
+                MeanKineticEnergy 
                 (
-                     strain_rate[n][i],
-                     K,
-                     turb_vecs,
-                     kraich_modes[n][i]
+                 (const MDConstants) K,
+                 (const Molecule**) positions,
+                 (const TurbField**) turb_velocities
                 );
-        } 
-          n++;
-    }
-    //file reading ends
-    //calc kinetic energy
-    double mean_kinetic_energy =
-        MeanKineticEnergy 
-        (
-         (const MDConstants) K,
-         (const Molecule**) positions,
-         (const TurbField**) turb_velocities
-        );
 
-     check
-	 (
+        check
+                (
 
-	 	!MeanStrainRateTensor
-        (
-             (const Tensor2**) strain_rate,
-             (const MDConstants) K,
-             meanS
-        ),
-		"MeanStrainRateTensor failed"
-	 );
+                 !MeanStrainRateTensor
+                 (
+                  (const Tensor2**) strain_rate,
+                  (const MDConstants) K,
+                  meanS
+                 ),
+                 "MeanStrainRateTensor failed"
+                );
 
-    for (unsigned k = 0; k < kDIM; ++k)
-    {
-        char message[50] = {0};
-        sprintf(message, "S[%u][%%u] = %%f\t", k);
-        PrintVals (message, meanS[k], kDIM);
-    }
+        for (unsigned k = 0; k < kDIM; ++k)
+        {
+                char message[50] = {0};
+                sprintf(message, "S[%u][%%u] = %%f\t", k);
+                PrintVals (message, meanS[k], kDIM);
+        }
 
-    fprintf (stdout, "Mean kinetic energy = %lf", mean_kinetic_energy);
+        fprintf (stdout, "Mean kinetic energy = %lf", mean_kinetic_energy);
 
-    FREE3D (kraich_modes, K.PartNum, K.SnapshotNum);
-
-    FREE2D (positions, K.SnapshotNum);
-    FREE2D (turb_velocities, K.SnapshotNum);
-    FREE2D (strain_rate, K.SnapshotNum);
-
-    FREE (turb);
-    FREE (turb_vecs);
-
-	return 0;
-
-    error:
-	{
         FREE3D (kraich_modes, K.PartNum, K.SnapshotNum);
 
         FREE2D (positions, K.SnapshotNum);
         FREE2D (turb_velocities, K.SnapshotNum);
         FREE2D (strain_rate, K.SnapshotNum);
 
-		FREE (turb);
-		FREE (turb_vecs);
+        FREE (turb);
+        FREE (turb_vecs);
 
-		return -1;
-	}
+        return 0;
+
+error:
+        {
+                FREE3D (kraich_modes, K.PartNum, K.SnapshotNum);
+
+                FREE2D (positions, K.SnapshotNum);
+                FREE2D (turb_velocities, K.SnapshotNum);
+                FREE2D (strain_rate, K.SnapshotNum);
+
+                FREE (turb);
+                FREE (turb_vecs);
+
+                return -1;
+        }
 }
 
